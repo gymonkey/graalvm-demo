093e89f4b669e36fcf77192d239d735bf77dd64a
liyiting <liyiting@lizhi.fm>
2024-08-27 18:18:32 +0800

    更新redis地址

--- src/main/java/app/nebula/java_worker_demo/client/JedisClient.java
+++ src/main/java/app/nebula/java_worker_demo/client/JedisClient.java
@@ -9,8 +9,8 @@ public class JedisClient {
 
     @Bean(name = "jedis")
     public Jedis getJedis(){
-        Jedis jedis = new Jedis("ruling-macaque-32179.upstash.io", 6379, false);
-        jedis.auth("AX2zAAIjcDE2MWYzMTEwNGFiMWU0NGI3OTUyY2I5YmU5NTFhMzc1ZnAxMA");
+        Jedis jedis = new Jedis("10.150.193.139", 6379, false);
+//        jedis.auth("AX2zAAIjcDE2MWYzMTEwNGFiMWU0NGI3OTUyY2I5YmU5NTFhMzc1ZnAxMA");
         return jedis;
     }
 
